90d4a17804be9d72e0ee8f6a39152ab817ef5b7e
SakiiR <sakiirlessons@gmail.com>
2021-12-09 09:05:01 +0100

    day 8 & day 9 templates

---
+++ aoc/day8/day.py
@@ -0,0 +1,16 @@
+from ..day import Day
+
+
+class Day8(Day):
+    name = "Day 8"
+    description = "Seven Segment Search"
+
+    def __init__(self, test=False) -> None:
+        self.getPaths(__file__)
+        super().__init__(test)
+
+    def part1(self):
+        return 0
+
+    def part2(self):
+        return 0
---
+++ aoc/day9/day.py
@@ -0,0 +1,16 @@
+from ..day import Day
+
+
+class Day9(Day):
+    name = "Day 9"
+    description = "Smoke Basin"
+
+    def __init__(self, test=False) -> None:
+        self.getPaths(__file__)
+        super().__init__(test)
+
+    def part1(self):
+        return 0
+
+    def part2(self):
+        return 0
