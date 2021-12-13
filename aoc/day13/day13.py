a2c20c7865c5562e9aa477d3e30f0c9c01db5c53
SakiiR SakiiR <sakiir@protonmail.com>
2021-12-13 14:45:58 +0100

    fold x

--- aoc/day13/day13.py
+++ aoc/day13/day13.py
@@ -1,5 +1,47 @@
-
+import enum
 from ..day import Day
+
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def fromLine(cls, line):
+        x, y = [int(x) for x in line.split(",")]
+        return cls(x, y)
+
+    def __repr__(self) -> str:
+        return f"Point(x={self.x}, y={self.y})"
+
+
+class FoldDirection(enum.Enum):
+    VERTICAL = "x"
+    HORIZONTAL = "y"
+
+
+class Cell(enum.Enum):
+    FILLED = "#"
+    EMPTY = "."
+
+
+class Fold:
+    def __init__(self, direction, value) -> None:
+        self.direction = direction
+        self.value = value
+
+    @classmethod
+    def fromLine(cls, line):
+        direction, value = line.split(" ")[-1].split("=")
+        value = int(value)
+
+        direction = FoldDirection(direction)
+
+        return cls(direction, value)
+
+    def __repr__(self) -> str:
+        return f"Fold(direction={self.direction}, value={self.value})"
 
 
 class Day13(Day):
@@ -10,11 +52,93 @@ class Day13(Day):
         self.getPaths(__file__)
         super().__init__(test)
 
+        self.lines = self.input_file_content.splitlines()
+
+        self.__points = []
+        self.__folds = []
+
+        self.__parse()
+
+        self.width, self.height = self.__determine_size()
+
+        self.__grid = [Cell.EMPTY for _ in range(self.width * self.height)]
+
+        for point in self.__points:
+            index = self.get_index_from_coord(point.x, point.y)
+            self.__grid[index] = Cell.FILLED
+
+    def fold(self, fold):
+        print(f"Folding {fold}")
+        if fold.direction == FoldDirection.HORIZONTAL:
+            return self.fold_y(fold.value)
+        if fold.direction == FoldDirection.VERTICAL:
+            return self.fold_x(fold.value)
+
+    def fold_x(self, value):
+        pass
+
+    def fold_y(self, value):
+        for y in range(value):
+            for x in range(self.width):
+                if self.at(x, y) == Cell.FILLED:
+                    delta = value - y
+                    # Empty the old value
+                    self.write(x, y, Cell.EMPTY)
+                    # Write the new value
+                    self.write(x, (value + delta), Cell.FILLED)
+
+    def get_coord_from_index(self, index):
+        x = index % self.width
+        y = int(index / self.width)
+        return x, y
+
+    def get_index_from_coord(self, x, y) -> int:
+        return x + (self.width * y)
+
+    def write(self, x, y, value: Cell):
+        self.__grid[self.get_index_from_coord(x, y)] = value
+        return value
+
+    def at(self, x, y) -> Cell:
+        return self.__grid[self.get_index_from_coord(x, y)]
+
+    def __parse(self):
+        for line in self.lines:
+            if "," in line:
+                self.__points.append(Point.fromLine(line))
+            if "fold" in line:
+                self.__folds.append(Fold.fromLine(line))
+
+    def __determine_size(self):
+        maxx = 0
+        maxy = 0
+
+        for pt in self.__points:
+            if pt.x > maxx:
+                maxx = pt.x
+            if pt.y > maxy:
+                maxy = pt.y
+
+        return maxx + 1, maxy + 1
+
+    def __repr__(self) -> str:
+        out = ""
+
+        for y in range(self.height):
+            out += f"{y:04d}. "
+            for x in range(self.width):
+                px = self.at(x, y)
+                out += px.value
+            out += "\n"
+        return out.rstrip("\n")
 
     def part1(self):
+
+        print(self)
+        self.fold(self.__folds[0])
+        print(self)
+
         return 0
 
-        
     def part2(self):
         return 0
-        
